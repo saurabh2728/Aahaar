cd3163d9b0ed64132cfb5d2663df76429eb5874f
unknown <10lalitsing01@gmail.com>
2026-05-04 02:17:46 +0530

    chore: disable ssr registration in generated route types

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -62,7 +62,7 @@ import type { getRouter } from './router.tsx'
 import type { createStart } from '@tanstack/react-start'
 declare module '@tanstack/react-start' {
   interface Register {
-    ssr: true
+    ssr?: false
     router: Awaited<ReturnType<typeof getRouter>>
   }
 }
